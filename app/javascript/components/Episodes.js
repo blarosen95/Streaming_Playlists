e5992d27bdf35d3e323fc76b5e6408fcacf2744c
Blake Rosenberg <barosenberg@bhmpc.com>
2021-08-29 09:39:22 -0400

    Episodes.js is grabbing first season episodes of passed prop showName values and listing them all out correctly. Initial implementation passing visual expectation testing so far! API call does not appear to be slowing the load time at the moment.
    Remove the remaining packs/components folder and the Episodes.vue component itself. Yarn remove executed on vue and vuetify
    
    Next up, will want to see the prop being passed in based on the current value of the "Show Name" field from form rather than hardcoded in the react_component tag.

--- app/javascript/components/Episodes.js
+++ app/javascript/components/Episodes.js
@@ -2,6 +2,18 @@ import React from "react"
 import PropTypes from "prop-types"
 import axios from "axios";
 
+// const [state, setState] = useState({
+//     headers: [
+//         {text: "Title", value: "Title"},
+//         {text: "Date", value: "Released"},
+//         {text: "Episode", value: "Episode"},
+//         {text: "IMDB ID", value: "imdbID"}
+//     ],
+//     showName: '',
+//     totalSeasons: 0,
+//     allEpisodes: []
+// });
+
 class Episodes extends React.Component {
     state = {
         headers: [
@@ -15,78 +27,67 @@ class Episodes extends React.Component {
         allEpisodes: []
     }
 
-    componentDidMount() {
-        this.setState(this.setTotalSeasons);
-        // console.log(this.state);
-        this.setAllEpisodes();
+    headers = [
+        {text: "Title", value: "Title"},
+        {text: "Date", value: "Released"},
+        {text: "Episode", value: "Episode"},
+        {text: "IMDB ID", value: "imdbID"}
+    ];
+    showName = '';
+    totalSeasons = 0;
+    allEpisodes = [];
+
+    async componentDidMount() {
+        await this.setTotalSeasons();
+        // console.log(this.state.totalSeasons);
+        await this.setAllEpisodes();
+        // console.log(this.state.allEpisodes)
     }
 
-    SetEps() {
-        axios.get(`http://www.omdbapi.com/?apikey=6c68744e&t=${this.props.showName}&type=series`)
-            .then(response => {
-                const totalSeasons = response.data.totalSeasons;
-                this.setState({totalSeasons});
-
-            })
+    async setTotalSeasons() {
+        await axios.get(`http://www.omdbapi.com/?apikey=6c68744e&t=${this.props.showName}&type=series`)
+            .then(function (response) {
+                this.setState({totalSeasons: response.data.totalSeasons});
+            }.bind(this))
             .catch(e => {
                 console.error(e);
             });
     }
 
-    // setTotalSeasons() {
-    //     axios.get(`http://www.omdbapi.com/?apikey=6c68744e&t=${this.props.showName}&type=series`)
-    //         .then(response => {
-    //             const totalSeasons = response.data.totalSeasons;
-    //             this.setState({totalSeasons});
-    //             // console.log(this.state);
-    //             return {totalSeasons};
-    //         })
-    //         .catch(e => {
-    //             console.error(e);
-    //         });
-    // }
+    async setAllEpisodes() {
+        // TODO: This is only going to grab Season 1 of a given show for now. Will deliberate on what/who should be looping this for us here.
 
-    setAllEpisodes() {
-        // This is only going to grab Season 1 of a given show for now. Will deliberate on what/who should be looping this for us here.
-        // console.log(this.state);
+        // TODO: Note that the current value for totalSeasons from the REST API is "13" for a show that will have a first episode in season 12 come out in roughly 1 month from now.
 
         // TODO: Interestingly enough, getting a given Season also lists the total seasons count in the response body! We may save a single API call knowing this.
-        if (this.totalSeasons) {
-            axios.get(`http://www.omdbapi.com/?apikey=6c68744e&t=${showName}&Season=${1}`)
-                .then(response => {
-                    // console.log(response.data.Episodes);
-                    const episodes = response.data.Episodes;
-                    this.setState({episodes});
-                    return this.state;
-                })
+        if (this.state.totalSeasons) {
+            await axios.get(`http://www.omdbapi.com/?apikey=6c68744e&t=${this.props.showName}&Season=${1}`)
+                .then(function (response) {
+                    this.setState({allEpisodes: response.data.Episodes});
+                }.bind(this))
                 .catch(e => {
                     console.error(e);
                 });
+        } else {
+            console.debug("Could not set state for allEpisodes. Else statement reached!");
+            // TODO: Revisit once the output of successful GETs are being utilized properly. We will then want this to provide our "sorry nothing found" verbiage and state values as needed.
         }
     }
 
     render() {
         return (
-            <ul>
-                {this.state.allEpisodes.map(episode => <li>{episode.Title}</li>)}
-            </ul>
-            // <React.Fragment>
-            //     Show Name: {this.props.showName}
-            // </React.Fragment>
+            <React.Fragment>
+                Show Name: {this.props.showName}
+                <ul>
+                    {this.state.allEpisodes.map(episode => <li>{episode.Title}</li>)}
+                </ul>
+            </React.Fragment>
         );
     }
 }
 
 Episodes.propTypes = {
     showName: PropTypes.string,
-    headers: [
-        {text: "Title", value: "Title"},
-        {text: "Date", value: "Released"},
-        {text: "Episode", value: "Episode"},
-        {text: "IMDB ID", value: "imdbID"}
-    ],
-    totalSeasons: 0,
-    allEpisodes: []
 };
 
 
