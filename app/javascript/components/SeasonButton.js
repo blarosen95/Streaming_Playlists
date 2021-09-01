5999b66f7dde597c4a6340873d99ef78e2fadea4
Blake Rosenberg <blarosen95@gmail.com>
2021-09-01 14:45:07 -0400

    Some stuff done but have an idea for next commit that changes how these seasons buttons exist entirely. Thinking a dropdown justified at end will work better.

--- app/javascript/components/SeasonButton.js
+++ app/javascript/components/SeasonButton.js
@@ -3,7 +3,7 @@ import React from "react";
 const SeasonButton = ({label, onButtonClick}) => (
     <div className="season-button">
         <div className="season-span-wrapper">
-            <span className="season-span btn-5" onClick={onButtonClick} id={"season-" + label}>&nbsp;&nbsp;{label}</span>
+            <span className="season-span btn-5" onClick={onButtonClick} id={"season-" + label}>{label}</span>
         </div>
     </div>
 );
