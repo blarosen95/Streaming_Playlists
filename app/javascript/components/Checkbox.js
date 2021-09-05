4d3f2d95f1f80f2345c498fb9b5bde11cd784e78
Blake Rosenberg <blarosen95@gmail.com>
2021-09-05 10:32:55 -0400

    Fixes the first item from issue #4, episode checkboxes now vertically align with their labels.

--- app/javascript/components/Checkbox.js
+++ app/javascript/components/Checkbox.js
@@ -2,16 +2,15 @@ import React from "react";
 
 const Checkbox = ({label, onBoxChange}) => (
     <div className="form-check">
-        <label className="episode-label">
-            {label}
-        </label>
         <input
             type="checkbox"
             name={label}
             onChange={onBoxChange}
             className="form-check-input episode-check"
         />
-        {/*</label>*/}
+        <label className="form-check-label episode-label">
+            {label}
+        </label>
     </div>
 );
 
